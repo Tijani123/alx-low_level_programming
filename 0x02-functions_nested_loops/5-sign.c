a1095c74ef871b4e5c5545ba002078054d5b7d04
Tijani123 <tundex838@gmail.com>
2022-04-14 01:04:11 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
@@ -24,6 +24,6 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar('-');
-		return ('1');
+		return ("-1");
 	}
 }
