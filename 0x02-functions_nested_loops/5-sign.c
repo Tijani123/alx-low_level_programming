3d1690ce61fcfb522f86061b338776784764f727
Tijani123 <tundex838@gmail.com>
2022-04-14 00:50:48 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,9 +5,10 @@
  * @n: The number of which the sign will be printed
  *
  * Return: 1 if the number is grater than zero,
- * 	   0 if the number is zero,
- * 	   -1 if the numbemis less than zero
+ *	    0 if the number is zero,
+ *	   -1 if the number is less than zero
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -15,9 +16,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n = 0)
 	{
 		_putchar ('0');
 		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return ('1');
 	}
 }
